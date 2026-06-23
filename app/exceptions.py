c439cff940275a2c2cb1aa7828cc4a34d79bf4d0
Roman Chirkov <chirkovroman2017@gmail.com>
2026-06-24 00:29:04 +1000

    add exceptions and dependencies

---
+++ app/exceptions.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
+
+from fastapi import status, HTTPException
+
+
+class TokenExpiredException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен истек")
+
+
+class TokenNoFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Токен не найден")
+
+
+UserAlreadyExistException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Пользователь уже существует")
+
+PasswordMismatchException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Пароли не совпадают")
+
+IncorrectEmailOrPasswordException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверная почта или пароль")
+
+NoJwtException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Не валидный токен")
+
+NoUserIdException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="ID пользователя не найден")
+
+ForbiddenException = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Недостаточно прав доступа")
