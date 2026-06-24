ebaf39865c58ba5c46243a10317e2337d9d3b3b4
Roman Chirkov <chirkovroman2017@gmail.com>
2026-06-25 00:56:20 +1000

    add auth html, js, css

--- app/users/router.py
+++ app/users/router.py
@@ -3,12 +3,19 @@ import email
 from fastapi import APIRouter, Response
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from app.exceptions import UserAlreadyExistException, IncorrectEmailOrPasswordException, PasswordMismatchException
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
 from app.users.schemas import SUserRegister, SUserAuth
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
+templates = Jinja2Templates(directory='app/templates')
+
+
+@router.get("/", response_class=HTMLResponse, summary="Страница авторизации")
+async def get_categories(request: Request):
+    return templates.TemplateResponse("auth.html", {"request": request})
 
 
 @router.post("/register/")
